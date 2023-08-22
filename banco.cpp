09dc7e278d926eeb3cdaaea09356469f2c4984a3
Adriel Ferreira Trajano <127546730+adriel1ft@users.noreply.github.com>
2023-08-22 01:37:20 -0300

    Transferência não está funcionando como eu quero

--- banco.cpp
+++ banco.cpp
@@ -56,12 +56,12 @@ void print_info(){
 };
 class Conta{
 int numero;//numero da conta
-Cliente *cli;
+Cliente* cli;
 float saldo;
 vector<Operacao> operacoes;
 
 public:
-Conta(int num, Cliente *c, float sa){
+Conta(int num, Cliente* c, float sa){
     numero = num;
     cli = c;
     saldo = sa;
@@ -70,12 +70,11 @@ Conta(int num, Cliente *c, float sa){
     operacoes.clear();
 }
 int get_numero(){return numero;}
-Cliente get_cliente(){return *cli;}
+Cliente* get_cliente(){return cli;}
 float get_saldo(){return saldo;}
-vector<Operacao> get_operacoes(){return operacoes;}
 
 void set_numero(int num){numero = num;}
-void set_cliente(Cliente *c){cli = c;}
+void set_cliente(Cliente* c){cli = c;}
 void set_saldo(float sa){saldo = sa;}
 void add_operacao(Operacao o){
     operacoes.push_back(o);
@@ -87,12 +86,17 @@ void add_operacao(Operacao o){
     }
 }
 
+
 void print_info(){
     cout << "Numero: " << numero << endl;
     cout << "Cliente: " << cli->get_nome() << endl;
     cout << "Operacoes: " << endl;
-    for (int i = 0; i < operacoes.size(); i++){
-        operacoes[i].print_info();
+   if (operacoes.size() == 0) {
+        cout << "\033[1m**Nenhuma.**\033[0m\n" << endl;
+    } else {
+        for (int i = 0; i < operacoes.size(); i++) {
+            operacoes[i].print_info();
+        }
     }
     cout << "Saldo: "<< saldo << endl;
 }
@@ -113,7 +117,7 @@ void add_conta(Conta c){
 void remove_conta(int n){ //
     for(int i=0; i < contas.size();i++){
         if(contas[i].get_numero() == n){
-            contas.erase(contas.begin()+1);
+            contas.erase(contas.begin()+i);
             break;
             
         }
@@ -130,35 +134,47 @@ Conta* get_conta(int n){
             return &contas[i];
         }
     }
-    return NULL;
+    return nullptr;
 }
 
-void transfere(Conta *c1, Conta *c2, float v){
-    Operacao o1('D', v);
-    Operacao o2('C', v);
-    c1->add_operacao(o1);
-    c2->add_operacao(o2);
+void transfere(int num1, int num2, float v){
+    cout << "Transferindo " << v << " de " << num1 << " para " <<num2 << endl;
+    Conta* c1 = get_conta(num1);
+    Conta* c2 = get_conta(num2);
+    
+    c1->add_operacao(Operacao('D', v));
+    c2->add_operacao(Operacao('C', v));
 }
+
+
 
 void print_info(){
     cout << "Banco: " << codigo << endl;
     cout << "Contas: " << endl;
     for(int i = 0; i < contas.size(); i++){
-        contas[i].print_info();}
+        contas[i].print_info();
+    cout << endl;}
+    
 }
+    
     
 };
 
 int main() {
 
-    Banco *b1 = new Banco(1);
     
-    Cliente *c1 = new Cliente("andre","158845677-00","rua das bananas");
-    Cliente *c2 = new Cliente("luiz","584345966-37","avenida ruy barbosa");
+    
+    Cliente c1("andre","158845677-00","rua das bananas");
+    Cliente c2("luiz","584345966-37","avenida ruy barbosa");
     //c1->print_info();
-    Conta *con1 = new Conta(12345678,c1,0);
-    Conta *con2 = new Conta(47885694,c2,100);
-        
+    Conta con1(12345678,&c1,0);
+    Conta con2(47885694,&c2,100);
+
+    Banco *b1 = new Banco(001);
+
+    b1->add_conta(con1);
+    b1->add_conta(con2);
+    
     Operacao op1('C',500);
     Operacao op2('D',330);
 
@@ -166,15 +182,16 @@ int main() {
     operacoes1.push_back(op1);
     operacoes1.push_back(op2);
 
-    con1->add_operacao(op1);
-    con1->add_operacao(op2);
-    con1->print_info();
+    con1.add_operacao(op1);
+    con1.add_operacao(op2);
+    //con1.print_info();
 
+    con2.add_operacao(op1);
+
+    b1->transfere(47885694, 12345678,50);
     
-
-    b1->transfere(con2, con1,50);
     b1->print_info();
 
-    
+   
     return 0;
 }
