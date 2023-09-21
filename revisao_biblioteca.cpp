c26822b46d559631b6035e23df650c041de27010
Adriel Ferreira Trajano <127546730+adriel1ft@users.noreply.github.com>
2023-09-21 11:36:57 -0300

    Atualizar o revisao_biblioteca.cpp

--- revisao_biblioteca.cpp
+++ revisao_biblioteca.cpp
@@ -23,6 +23,12 @@ public:
     virtual void exibir()=0
 };
 
+friend ostream& operator<<(ostream& os, usuario u){
+os << u.nome << ", " << u.idade << endl; //concatenar informacoes
+
+virtual void tipo_usuario(){//completar}
+}
+
 class estudante: public usuario{
     int matricula;
     string curso;
@@ -37,6 +43,13 @@ public:
     
     string get_curso(){return curso;}
     void set_curso(const string c){curso=c;}
+
+    vois exibir(){ //print na tela
+    cout << Nome << get.nome() << endl;
+
+friend ostream& operator<(ostream& os, estudante e){
+//completar
+}
 };
 
 class professor: public usuario{
@@ -44,6 +57,7 @@ class professor: public usuario{
 
 public:
     professor(const string& n, int i, const string& d): usuario(n,i), dep(d){}
+
 };
 
 class material{
@@ -55,9 +69,8 @@ public:
     string get_titulo(){return titulo;}
     void set_titulo(const string& t){titulo =  t;}
 
-virtual void exibir_detalhes(){ // nas subclasses ela detalha tbm
-    cout << "titulo: " << titulo << endl;
-}
+virtual void exibir_detalhes() = 0;// nas subclasses ela detalha tbm
+};
 
 class livro: public material{
     string autor;
@@ -67,8 +80,8 @@ public:
     livro(const string& t,const string& au,int ano): material(t), autor(a),ano_publicacao(ano){}
     
     //gets e sets
-}
 
+void exibir(){//completar}
 friend ostream& operator>>(ostream& os,const material& ma){
     os << ma.titulo << endl;
     return os;
@@ -80,26 +93,53 @@ class revista: public material{
     int num_edicao;
     
 public:
-    livro(const string& t,int e): material(t), num_edicao(e){}
+    revista(const string& t,int e): material(t), num_edicao(e){}
     
     //gets e sets
+friend ostream& operator<<(ostream& os, revista r){
+//completar
 }
+};
 
 class emprestimo{
-    usuario usu;
-    material mat;
+    usuario* u;
+    material* m;
+    int qtd_material;
+    string data;
 
 public:
-    emprestimo()
-    
+    emprestimo(usuario uu, material mm, int q, const string& d{}
+
+//gets e sets 
+
+emprestimo& operator+(emprestimo& outro) //primeiro operando é ele q recebe. em ostream começa com ostream. é fora
+    {if(u==outro.u)
+// criar novo array que eh a soma dos dos anteriores
+{novo_q = qtd_m + outro.qtd_m;
+material* na = malloc(novi_q*sizeof(material);
+// preencher (popular) 'na' com emprestimos}
+}
+
+//friend ostream
 };
 
 class biblioteca{
+
+    //pode declarar assim ou na mão: classe, int qtd
+    /*vector traz facilidades mas pode acontecer de
+fazer na mão ser mais rapido e consumir menos memória*/
+
     vector<usuario*> usuarios;
     vector<material*> materiais;
     vector<emprestimo*> emprestimos;
     
 public:
+
+    biblioteca (){
+    usuarios = null;
+    
+
+}
     void adicionar_usuario(usuario* usr){
         usuarios.push_back(usr);
     }
@@ -123,8 +163,4 @@ public:
 
 };
 
-class livro: public material{};
-
-class revista: public material{};
-
-class emprestimo{};
+// rever oq o professor fez de diferente na sala
