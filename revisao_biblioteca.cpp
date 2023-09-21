e38e024ad70ebebc27c867c618c26d09bfc12d92
Adriel Ferreira Trajano <127546730+adriel1ft@users.noreply.github.com>
2023-09-21 10:24:26 -0300

    Update revisao_biblioteca.cpp

--- revisao_biblioteca.cpp
+++ revisao_biblioteca.cpp
@@ -29,6 +29,12 @@ public:
     estudante(const string& n, int i, int m, const string& c): usuario(n,i), matricula(m), curso(c){}
 
     //gets e sets
+    
+    int get_matricula(){return matricula;}
+    void set_matricula(int m){matricula = m;}
+    
+    string get_curso(){return curso;}
+    void set_curso(const string c){curso=c;}
 };
 
 class professor: public usuario{
@@ -47,14 +53,72 @@ public:
     string get_titulo(){return titulo;}
     void set_titulo(const string& t){titulo =  t;}
 
-virtual void exibir_detalhes(){
+virtual void exibir_detalhes(){ // nas subclasses ela detalha tbm
     cout << "titulo: " << titulo << endl;
+}
+
+class livro: public material{
+    string autor;
+    int ano_publicacao;
+
+public:
+    livro(const string& t,const string& au,int ano): material(t), autor(a),ano_publicacao(ano){}
+    
+    //gets e sets
 }
 
 friend ostream& operator>>(ostream& os,const material& ma){
     os << ma.titulo << endl;
     return os;
 }
+
+};
+
+class revista: public material{
+    int num_edicao;
+    
+public:
+    livro(const string& t,int e): material(t), num_edicao(e){}
+    
+    //gets e sets
+}
+
+class emprestimo{
+    usuario usu;
+    material mat;
+
+public:
+    emprestimo()
+    
+};
+
+class biblioteca{
+    vector<usuario*> usuarios;
+    vector<material*> materiais;
+    vector<emprestimo*> emprestimos;
+    
+public:
+    void adicionar_usuario(usuario* usr){
+        usuarios.push_back(usr);
+    }
+    
+    void adicionar_material(material* mat){
+        materiais.push_back(mat);
+    }
+    
+    void listar_usuarios(){
+        cout << "Lista de usuarios:" << endl;
+        for(const auto& usr: usuarios){
+            cout << "Nome: " << usr->get_nome << endl;
+        }
+    }
+    
+    void emprestar_material(usuario* usr, material* mat){
+        emprestimos.push_back(usr);
+    }
+};
+
+
 };
 
 class livro: public material{};
