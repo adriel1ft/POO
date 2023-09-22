dd3cb0a5036c4bf577950c0c20bb85792d9e2da2
Adriel Ferreira Trajano <127546730+adriel1ft@users.noreply.github.com>
2023-09-22 02:14:08 -0300

    need to fix emprestimo and finalize biblioteca

--- revisao_biblioteca.cpp
+++ revisao_biblioteca.cpp
@@ -18,16 +18,17 @@ public:
     string get_nome(){return nome;}
     void set_nome(const string& n){nome =  n;}
     int get_idade(){return idade;}
-    void get_nome(int i){idade = i;}
+    void set_idade(int i){idade = i;}
 
-    virtual void exibir()=0
-};
+    virtual void exibir()=0;
 
-friend ostream& operator<<(ostream& os, usuario u){
-os << u.nome << ", " << u.idade << endl; //concatenar informacoes
+    friend ostream& operator<<(ostream& os, usuario& u){
+    os << u.nome << ", " << u.idade << endl; //concatenar informacoes
+    }
 
-virtual void tipo_usuario(){//completar}
+virtual void tipo_usuario(){//completar
 }
+};
 
 class estudante: public usuario{
     int matricula;
@@ -44,11 +45,13 @@ public:
     string get_curso(){return curso;}
     void set_curso(const string c){curso=c;}
 
-    vois exibir(){ //print na tela
-    cout << Nome << get.nome() << endl;
+    void exibir() override { //print na tela
+    cout << "Nome: " << get_nome() << ", Matrícula: " << matricula << ", Curso: " << curso << endl;}
 
-friend ostream& operator<(ostream& os, estudante e){
-//completar
+friend ostream& operator<<(ostream& os, estudante& e){
+
+    os << "Nome: " << e.get_nome() << ", Matrícula: " << e.matricula << ", Curso: " << e.curso << endl;
+    return os;
 }
 };
 
@@ -58,6 +61,18 @@ class professor: public usuario{
 public:
     professor(const string& n, int i, const string& d): usuario(n,i), dep(d){}
 
+    string get_dep(){return dep;}
+    void set_dep(const string& d){dep = d;}
+
+    void exibir() override { //print na tela
+    cout << "Nome: " << get_nome() << ", Departamento: " << dep << endl;}
+
+    friend ostream& operator<<(ostream& os, professor& p){
+
+        os << "Nome: " << p.get_nome() << ", Departamento: " << p.dep << endl;
+        return os;
+}
+
 };
 
 class material{
@@ -66,10 +81,10 @@ class material{
 public:
     material(const string& t):titulo(t){}
 
-    string get_titulo(){return titulo;}
+    string get_titulo() const {return titulo;}
     void set_titulo(const string& t){titulo =  t;}
 
-virtual void exibir_detalhes() = 0;// nas subclasses ela detalha tbm
+virtual void exibir() = 0;// nas subclasses ela detalha tbm
 };
 
 class livro: public material{
@@ -77,13 +92,21 @@ class livro: public material{
     int ano_publicacao;
 
 public:
-    livro(const string& t,const string& au,int ano): material(t), autor(a),ano_publicacao(ano){}
+    livro(const string& t,const string& au,int ano): material(t), autor(au),ano_publicacao(ano){}
     
     //gets e sets
+    string& get_autor(){return autor;}
+    void set_autor(const string& au){autor = au;}
+    int get_ano_publicacao(){return ano_publicacao;}
+    void set_ano_publicacao(int ano){ano_publicacao = ano;}
 
-void exibir(){//completar}
-friend ostream& operator>>(ostream& os,const material& ma){
-    os << ma.titulo << endl;
+
+void exibir() override {
+    cout << "Titulo: " << get_titulo() << ", Autor: " << autor << ", Ano de Publicacao: " << ano_publicacao << endl;
+}//completar
+
+friend ostream& operator<<(ostream& os,const livro& li){
+    os << "Titulo: " << li.get_titulo() << ", Autor: " << li.autor << ", Ano de Publicacao: " << li.ano_publicacao << endl;
     return os;
 }
 
@@ -96,7 +119,13 @@ public:
     revista(const string& t,int e): material(t), num_edicao(e){}
     
     //gets e sets
-friend ostream& operator<<(ostream& os, revista r){
+    int get_num_edicao(){return num_edicao;}
+    void set_num_edicao(int e){num_edicao = e;}
+
+    void exibir() override{}
+
+
+friend ostream& operator<<(ostream& os, revista& r){
 //completar
 }
 };
@@ -108,17 +137,43 @@ class emprestimo{
     string data;
 
 public:
-    emprestimo(usuario uu, material mm, int q, const string& d{}
+    emprestimo(usuario* uu, material* mm, int q, const string& d):u(uu),m(mm),qtd_material(q),data(d){}
 
 //gets e sets 
 
-emprestimo& operator+(emprestimo& outro) //primeiro operando é ele q recebe. em ostream começa com ostream. é fora
-    {if(u==outro.u)
-// criar novo array que eh a soma dos dos anteriores
-{novo_q = qtd_m + outro.qtd_m;
-material* na = malloc(novi_q*sizeof(material);
-// preencher (popular) 'na' com emprestimos}
+int get_qtd_material(){return qtd_material;}
+void set_qtd_material(int q){qtd_material = q;}
+
+emprestimo& operator+(emprestimo& outro){ //primeiro operando é ele q recebe. em ostream começa com ostream. é fora
+    if(u==outro.u){// criar novo array que eh a soma dos dos anteriores
+
+        int novo_q = qtd_material + outro.qtd_material;
+
+       material* na = new material[novo_q]; // alocando memoria para o novo array
+
+        //Copiar os materiais originais para o array 'na'
+        for(int i = 0; i < qtd_material; i++){
+            na[i] = m[i];
+        }
+
+        // Copiar os materiais do empréstimo 'outro' para o array 'na'
+        for (int i = 0; i < outro.qtd_material; ++i) {
+            na[qtd_material + i] = outro.m[i];
+        }
+
+        //liberar memoria
+        delete[] m;
+
+        //atualizar o ponteiro 'm' para apontar para p novo array 'na'
+        m = na;
+
+
 }
+return *this; // Retorne o próprio objeto para suportar operações em cadeia (por exemplo, emp1 + emp2 + emp3)
+}
+    
+
+
 
 //friend ostream
 };
@@ -161,6 +216,10 @@ public:
 };
 
 
-};
+int main(){
+
+
+    return 0;
+}
 
 // rever oq o professor fez de diferente na sala
